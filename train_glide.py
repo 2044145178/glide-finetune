7ddcc068bb787b04427e262f7fb64eedb6172f68
2044145178 <20441455178@qq.com>
2024-09-25 16:51:34 +0800

    :zap: 增加DDP训练

--- train_glide.py
+++ train_glide.py
@@ -87,8 +87,8 @@ def run_glide_finetune(
         activation_checkpointing=activation_checkpointing,
         model_type="base" if not enable_upsample else "upsample",
     )
-    if th.cuda.is_available():
-        glide_model = _CustomDataParallel(glide_model)
+    # if th.cuda.is_available():
+    #     glide_model = _CustomDataParallel(glide_model)
 
     glide_model.train()
     number_of_params = sum(x.numel() for x in glide_model.parameters())
